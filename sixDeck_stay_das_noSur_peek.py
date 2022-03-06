41d24bfb34958bb35e4dbd62ce2388600ab79260
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2022-03-05 17:26:47 -0700

    Debugging and working on manual play

--- sixDeck_stay_das_noSur_peek.py
+++ sixDeck_stay_das_noSur_peek.py
@@ -7,6 +7,9 @@ Created on Sat Mar  5 11:32:15 2022
 
 from actions import H, S, P, D, DS
 
+# Rules source:
+# https://www.blackjackinfo.com/blackjack-basic-strategy-engine/?numdecks=6&soft17=s17&dbl=all&das=yes&surr=ns&peek=yes
+
 deck_num = 6
 doubles = 'any 2 cards'
 surrender = False
@@ -14,7 +17,23 @@ soft_17 = 'dealer stands'
 double_after_split = True
 dealer_hole_peek = True
 
+# The dealer's visible card, used to index tables
 dealer = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+
+# The dealer's action table
+def dealer_action(hand):
+    '''Returns "bust" if busted, card total if standing, or "hit" if hitting'''
+    if sum(hand) > 21:
+        return 'bust'
+    elif 1 in hand and 7 <= sum(hand) <= 11:
+        # Dealer stands on 17+ if soft
+        return sum(hand) + 10
+    elif 17 <= sum(hand):
+        return sum(hand)
+    else:
+        return 'hit'
+
+# Sum of hand
 hard_totals = {
      4: [H, H, H, H, H, H, H, H, H, H],
      5: [H, H, H, H, H, H, H, H, H, H],
@@ -33,10 +52,10 @@ hard_totals = {
     18: [S, S, S, S, S, S, S, S, S, S],
     19: [S, S, S, S, S, S, S, S, S, S],
     20: [S, S, S, S, S, S, S, S, S, S],
-    21: [S, S, S, S, S, S, S, S, S, S],
+    # 21 <-- WIN, so key not needed
     }
 
-# i.e. Ace + Number
+# Ace + sum(other cards)
 soft_totals = {
     2: [H, H, H, D, D, H, H, H, H, H],
     3: [H, H, H, D, D, H, H, H, H, H],
@@ -46,8 +65,20 @@ soft_totals = {
     7: [S, DS, DS, DS, DS, S, S, H, H, H],
     8: [S, S, S, S, S, S, S, S, S, S],
     9: [S, S, S, S, S, S, S, S, S, S],
+    # 10 <-- WIN (A=11 + 10), so key not needed
+   11: [H, H, S, S, S, H, H, H, H, H],
+   12: [S, S, S, S, S, H, H, H, H, H],
+   13: [S, S, S, S, S, H, H, H, H, H],
+   14: [S, S, S, S, S, H, H, H, H, H],
+   15: [S, S, S, S, S, H, H, H, H, H],
+   16: [S, S, S, S, S, S, S, S, S, S],
+   17: [S, S, S, S, S, S, S, S, S, S],
+   18: [S, S, S, S, S, S, S, S, S, S],
+   19: [S, S, S, S, S, S, S, S, S, S],
+   # 20 <-- WIN (A=1 + 20), so key not needed
     }
 
+# Hand is exactly two cards, and they're the same
 pairs = {
       (2,2): [P, P, P, P, P, P, H, H, H, H],
       (3,3): [P, P, P, P, P, P, H, H, H, H],
